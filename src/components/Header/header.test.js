9b15d0f139c4f22f99d840ac2efbc391050107f0
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-04-05 21:04:49 +0500

    Add more Tests

---
+++ src/components/Header/header.test.js
@@ -0,0 +1,13 @@
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './header';
+
+test("Snapshot Testing of Header.js", () => {
+  const comp = renderer.create(
+    <Router>
+    <Header />
+    </Router>
+  );
+  let tree = comp.toJSON();
+  expect(tree).toMatchSnapshot();
+});
